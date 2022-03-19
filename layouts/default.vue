d3f91b4a702d36a463dfce5246ed28530c4d5f86
snozaki <snozaki@netprotections.co.jp>
2022-03-19 14:29:23 +0900

    MOD: 状態管理をルートコンポーネントで管理

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,15 @@
         <DefaultHooter />
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, provide } from '@nuxtjs/composition-api'
+import { TodoItemRepository } from '@/composables/repository/TodoItemRepository'
+import { TodoItemKey, InMemoryTodoItemRepository } from '~/composables/repository/InMemoryTodoItemRepository'
+
+export default defineComponent({
+    setup() {
+        provide<TodoItemRepository>(TodoItemKey, new InMemoryTodoItemRepository())
+    },
+})
+</script>
